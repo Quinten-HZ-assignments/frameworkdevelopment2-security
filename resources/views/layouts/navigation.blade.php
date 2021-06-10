d2abff007a8e8759e3a751d3b9e6b1ad3d299937
Quinten <70522000+awqfrxx@users.noreply.github.com>
2021-06-11 01:27:30 +0200

    Fixed naming conventions again

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,7 +23,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('blogs')" :active="request()->routeIs('blogs')">
-                        {{ __('blogs') }}
+                        {{ __('Blogs') }}
                     </x-nav-link>
                 </div>
                 @auth()
